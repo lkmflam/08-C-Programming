5cb446c31e3094dfe0e9d6b889d9825649488c24
lkmflam <53613483+lkmflam@users.noreply.github.com>
2020-01-09 15:24:08 -0600

    Add files via upload

---
+++ 05_Operators_expressions/Lab9Attempt.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+# include <math.h>
+
+int main(void)
+{
+int a = 0;
+int b = 0;
+int c = 0;
+printf("Type the length of the the first side/ side A: ");
+scanf("%d", a);
+printf("Type the length of the the first side/ side B: ");
+scanf("%d", b);
+a^2 + b^2 = c;
+double sqrt(double c);
+printf("The sides of the triangle measure: %d %d %d \n", a, b, c);
+}
