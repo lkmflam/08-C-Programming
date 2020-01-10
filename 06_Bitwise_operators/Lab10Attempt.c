9a56e7cced8664385fe33b2a4e09fd0894214ef5
lkmflam <53613483+lkmflam@users.noreply.github.com>
2020-01-10 14:31:39 -0600

    Add files via upload

---
+++ 06_Bitwise_operators/Lab10Attempt.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+int main(void)
+{
+    uint32_t x = 0;
+    printf("Please enter the integer: ");
+    fscanf(stdin, "%d", &x);
+    
+}
---
+++ 06_Bitwise_operators/StudentPerformanceLab1.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+int main(void)
+{
+    int PerformanceLab1();
+    {
+        int x = 0;
+        printf("Please enter the integer: ");
+        scanf("%d", &x);
+        printf("\nx << 3 = %d\n", x << 3);
+    }
+    int PerformanceLab2();
+    {
+        int y = 0;
+        printf("Please enter the integer: ");
+        scanf("%d", &y);
+        printf("\ny >> 5 = %d\n", y >> 5);
+    }
+}
